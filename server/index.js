9750d111dd6c1341bf1a818084b993b2e6dfb29b
Timothy <Rorie.TimothyJr@gmail.com>
2022-04-06 22:35:18 -0400

    Start server init

--- server/index.js
+++ server/index.js
@@ -2,3 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+
