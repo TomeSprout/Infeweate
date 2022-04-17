2fdeb54c003fb8ac0ff9bdb5f1b52630bbd9f06f
Timothy <Rorie.TimothyJr@gmail.com>
2022-04-17 18:42:36 -0400

    Add WIP for user flow in Reservation setter

--- client/src/Components/AppShell/ApplicationShell.tsx
+++ client/src/Components/AppShell/ApplicationShell.tsx
@@ -1,10 +1,50 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AppShell, Navbar, Header, Footer, Text, MediaQuery, Burger, useMantineTheme } from '@mantine/core';
+import {
+	AppShell,
+	Box, 
+	Burger, 
+	Button, 
+	Checkbox, 
+	Footer, 
+	Group,
+	Header, 
+	MediaQuery, 
+	Navbar, 
+	Select, 
+	Text, 
+	TextInput, 
+	useMantineTheme, 
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+type Locales = {
+	id: number;
+	city: string;
+	state: string;
+}
 
 const ApplicationShell = () => {
 	const theme = useMantineTheme();
 	const [ opened, setOpened ] = useState(false);
+
+	const data = ['sd', 'dsds', 'sdds']
+
+	const locationData: Locales[] = [
+		{ id: 0, city: 'New York', state: 'NY' },
+	];
+
+	const form = useForm({
+		initialValues: {
+			email: '',
+			termsOfService: false,
+		},
+
+		validate: {
+			email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+		},
+	});
+
 	return (
 		<Router>
 			<AppShell
@@ -13,10 +53,10 @@ const ApplicationShell = () => {
 						background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
 					},
 				}}
-				navbarOffsetBreakpoint="sm"
+				navbarOffsetBreakpoint='sm'
 				fixed
 				navbar={
-					<Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+					<Navbar p='md' hiddenBreakpoint='sm' hidden={!opened} width={{ sm: 200, lg: 300 }}>
 						<Navbar.Section>
 							<Text>Navbar Main</Text>
 						</Navbar.Section>
@@ -29,15 +69,15 @@ const ApplicationShell = () => {
 					</Navbar>
 				}
 				footer={
-					<Footer height={60} p="md">
+					<Footer height={60} p='md'>
 						Application footer
 					</Footer>
 				}
 				header={
-					<Header height={70} p="md">
+					<Header height={70} p='md'>
 						<div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-							<MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-								<Burger opened={opened} onClick={() => setOpened((o) => !o)} size="sm" color={theme.colors.gray[6]} mr="xl" />
+							<MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+								<Burger opened={opened} onClick={() => setOpened((o) => !o)} size='sm' color={theme.colors.gray[6]} mr='xl' />
 							</MediaQuery>
 
 							<Text>Application header</Text>
@@ -46,7 +86,48 @@ const ApplicationShell = () => {
 				}
 			>
 				<Routes />
-					<Text>Resize app to see responsive navbar in action</Text>
+				DEFAULTS:
+				Location: New York, NY
+
+				<Box sx={{ maxWidth: 500 }} mx='auto'>
+					
+					<Select
+						data={data}
+						placeholder="Pick a location"
+						label="Where are you eating?"
+						variant="filled"
+						radius="md"
+						size="md"
+						required
+					/>
+
+					<form onSubmit={form.onSubmit((values) => console.log(values))}>
+						
+						<TextInput
+							required
+							label='Email'
+							radius='md'
+							size='md'
+							placeholder='email@emailprovider.com'
+							{...form.getInputProps('email')}
+						/>
+
+						<Checkbox
+							mt='md'
+							label='Water balloons'
+							{...form.getInputProps('termsOfService', { type: 'checkbox' })}
+						/>
+
+						<Group position='right' mt='md'>
+							<Button type='submit'>Submit</Button>
+						</Group>
+						
+						<Group position='right' mt='md'>
+							<Button type='reset'>Reset</Button>
+						</Group>
+
+					</form>
+				</Box>
 			</AppShell>
 		</Router>
 	);
